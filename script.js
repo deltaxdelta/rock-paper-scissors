129838d4bb9fb4824ea960780270496e7be703d7
deltaxdelta <dd.deniseduke@gmail.com>
2021-07-20 15:29:06 -0400

    Put in some variables and scoring in playRound

--- script.js
+++ script.js
@@ -86,67 +86,92 @@ function playRound (playerSelection, computerSelection) {
   
 
   const x = computerSelection();
+
+  let playerScore = 0;
+  let computerScore = 0;
   
   // console.log(x);
   console.log('You throw ' + playerSelection + '.');
  
   if (x == playerSelection) {
     console.log('A tie! Try again.');
-   // return 'A tie! Try again.'
+    return (playerscore + 0, computerScore + 0);
   }
 
   else if (x == 'rock' && playerSelection == 'paper') {
     console.log('Computer throws rock. You win!');
-   // return 'Computer throws rock. You win!';
+    return (playerScore + 1, computerScore + 0);
   }
  
   else if (x == 'paper' && playerSelection == 'scissors') {
     console.log('Computer throws rock. You win!');
-   // return 'Computer throws paper. You win!';
+    return (playerScore + 1, computerScore + 0);
   }
  
   else if (x == 'scissors' && playerSelection == 'rock') {
     console.log('Computer throws scissors. You win!'); 
-  //  return 'Computer throws scissors. You win!'
+    return (playerScore + 1, computerScore + 0);
   }
  
   else if (x == 'paper' && playerSelection == 'rock') {
     console.log('Computer throws paper. You lose!');
-   // return 'Computer throws paper. You lose!'
+    return (playerScore + 0, computerScore + 1);
   }
 
   else if (x == 'rock' && playerSelection == 'scissors') {
     console.log('Computer throws rock. You lose!');
-   // return 'Computer throws rock. You lose!'
+    return (playerScore + 0, computerScore + 1);
   }
 
   else if (x == 'scissors' && playerSelection == 'paper') {
     console.log('Computer throws scissors. You lose!');
-   // return 'Computer throws scissors. You lose!'
+    return (playerScore + 0, computerScore + 1);
   }
   else { 
     console.log('You throw' + playerSelection + '. That\'s not part of the game!');
-   // return 'Yes, those are different things.'
+    return (console.log('Why did you throw that? Computer gets 2 points.'), computerscore + 2);
   }
 }
 
 // one day I will know what a loop is
+
 function game () {
 let y = window.prompt('Rock, paper, or scissors?');
+
  playRound(y, computerSelection);
 
  let z = window.prompt('Rock, paper, or scissors?');
+
  playRound(z, computerSelection);
 
  let a = window.prompt('Rock, paper, or scissors?');
+
  playRound(a, computerSelection);
 
  let b = window.prompt('Rock, paper, or scissors?');
+
  playRound(b, computerSelection);
 
  let c = window.prompt('Rock, paper, or scissors?');
+
  playRound(c, computerSelection);
 
 }
 
+// how to do scoring and winner declaration?
 
+/*
+Create a variable, playerScore
+Create a variable, computerScore
+
+check results of each playRound -- should go in game function, right?
+  how do I get the result? --where does that go?
+   can I return 1 + playerScore/computerScore w/ each message?
+ if there's a 'you win' result, +1 to playerscore
+ if there's a 'you lose' result +1 to computerScore
+ else +0 to both (tie)
+
+compare playerScore to computerScore - display them too for funsies
+ largest value is winner
+Display finalScore, plus computer or player winning message
+*/ 
